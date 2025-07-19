8ec76569dbebec510639070abba46b8c8c87a530
Aneenasebastian <aneenasebastian2028@cs.sjcetpalai.ac.in>
2025-07-19 21:01:16 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,14 @@
+package module1;
+
+public class Even {
+	
+		public static void main(String[] args) {
+		System.out.println("The even number from 1 to 20 is :");{
+			for(int i=2;i<=20;i+=2) {
+				System.out.println(i);
+			}
+		}
+
+	
+		}
+}
